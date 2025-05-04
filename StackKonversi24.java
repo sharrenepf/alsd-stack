6d569ffed829037fe3692ee4e60919519d6b19fb
Sharren <sharrenepf@gmail.com>
2025-05-04 18:08:44 +0700

    percobaan

---
+++ StackKonversi24.java
@@ -0,0 +1,47 @@
+public class StackKonversi24 {
+ 
+    int[] tumpukanBiner;
+    int size, top;
+
+    public StackKonversi24(int size) {
+        this.size = 32;
+        tumpukanBiner = new int[size];
+        top = -1;
+    }
+
+    public boolean isFull() {
+        if (top == size - 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isEmpty() {
+        if (top == -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void push(int data) {
+        if (isFull()) {
+            System.out.println("Stack penuh");
+        } else {
+            top++;
+            tumpukanBiner[top] = data;
+        }
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack kosong");
+            return -1;
+        } else {
+            int data = tumpukanBiner[top];
+            top--;
+            return data;
+        }
+    }
+}
